fa0bbcebf456497e64c38295ccf872b2641624a6
fritzmg <fmg@inspiredminds.at>
2024-06-10 09:07:07 +0100

    add missing German translation

--- contao/languages/de/tl_calendar_events.php
+++ contao/languages/de/tl_calendar_events.php
@@ -18,4 +18,6 @@ $GLOBALS['TL_LANG']['tl_calendar_events']['reg_max'] = ['Maximale Anzahl', 'Eine
 $GLOBALS['TL_LANG']['tl_calendar_events']['reg_regEnd'] = ['Ende der Registrierung', 'Optionales Datum des Ende der Registrierungsmöglichkeit.'];
 $GLOBALS['TL_LANG']['tl_calendar_events']['reg_cancelEnd'] = ['Ende der Stornierung', 'Optionales Datum des Ende der Stornierungsmöglichkeit.'];
 $GLOBALS['TL_LANG']['tl_calendar_events']['reg_requireConfirm'] = ['Bestätigung erforderlich', 'Zählt nur bestätigte Registrierungen.'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['reg_enableWaitingList'] = ['Warteliste aktivieren', 'Aktiviert die Warteliste für Anmeldungen nach dem Maximum.'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['reg_waitingListAdvancementNotification'] = ['Benachrichtigung für Aufstrieg von Warteliste', 'Diese Benachrichtitung wird versendet wenn eine Teilnehmer*in von der Warteliste auf die reguläre Liste der Anmeldungen aufsteigt.'];
 $GLOBALS['TL_LANG']['tl_calendar_events']['registrations'] = 'Registrierungen';
