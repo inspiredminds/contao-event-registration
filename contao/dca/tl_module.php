da1692d4adba8270ea17f5691b8f87de29a806c5
fritzmg <fmg@inspiredminds.at>
2025-02-24 10:32:51 +0000

    wip

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -6,7 +6,9 @@ declare(strict_types=1);
  * (c) INSPIRED MINDS
  */
 
+use Contao\Controller;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
 use InspiredMinds\ContaoEventRegistration\Controller\FrontendModule\EventRegistrationCalendarController;
 use InspiredMinds\ContaoEventRegistration\Controller\FrontendModule\EventRegistrationCancelController;
 use InspiredMinds\ContaoEventRegistration\Controller\FrontendModule\EventRegistrationConfirmController;
@@ -22,6 +24,35 @@ $GLOBALS['TL_DCA']['tl_module']['palettes'][EventRegistrationCalendarController:
 $class = $GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] ?? '';
 $GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] = trim($class.' clr');
 
+$GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo_er'] = [
+    'exclude' => true,
+    'inputType' => 'pageTree',
+    'foreignKey' => 'tl_page.title',
+    'eval' => ['fieldType' => 'radio'],
+    'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0],
+    'relation' => ['type' => 'hasOne', 'load' => 'lazy'],
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['cal_ctemplate_er'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['cal_ctemplate'],
+    'exclude' => true,
+    'inputType' => 'select',
+    'options_callback' => static fn () => Controller::getTemplateGroup('cal_event_registration'),
+    'eval' => [
+        'includeBlankOption' => true,
+        'chosen' => true,
+        'tl_class' => 'w50',
+    ],
+    'sql' => [
+        'type' => 'text',
+        'length' => MySQLPlatform::LENGTH_LIMIT_TINYTEXT,
+        'notnull' => false,
+    ],
+];
+
 PaletteManipulator::create()
+    ->addField('cal_ctemplate_er', 'cal_ctemplate')
+    ->removeField('cal_ctemplate')
+    ->addField('jumpTo_er', 'jumpTo')
     ->applyToPalette(EventRegistrationCalendarController::TYPE, 'tl_module')
 ;
